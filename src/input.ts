05b7cd767ce73135f0ab437a271a4f096396313e
Bobby Dusk <bobby@dusk.monster>
2025-01-31 09:40:53 +0100

    forgot to add input file

---
+++ src/input.ts
@@ -0,0 +1,10 @@
+import { Direction } from "./app"
+
+document.addEventListener("keydown", (event: KeyboardEvent) => {
+  if (event.key === "j") {
+
+  }
+})
+
+function press(direction: Direction) {
+}
