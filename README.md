f941e64509920d47daf00068d02befaf5621b430
Bobby Dusk <bobby@dusk.monster>
2025-01-31 09:40:04 +0100

    adds info

--- README.md
+++ README.md
@@ -8,3 +8,14 @@ Dance dance revolution combined with navigating a maze. You're only allowed to m
 
 - Speed is defined by BPM.
 - The smallest precision is 1/16 of a beat.
+
+## JSON song file
+
+- *notes* - time in ms, direction with 0 = left, 1 = down, 2 = up, 3 = right
+
+## TODO
+
+- Have a deteministic play field, i.e. based on the time elapsed since start song, the beats and subbeats since start are calculated. Therefore the beat values of the lines are updated every loop. The arrows are then simply mapped onto the field.
+- No queue. Just have an index of the first relevant arrow. Then, go through the arrows until you've reached an arrow that is not yet relevant
+- Have a polling system that is independant of the rendering loop. Then graphics might render at 60 pfs, but the inputs are actually checked at 120 or 240 fps.
+- Open source fps display.
--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,27 @@
-import { Application, Assets, Sprite, Ticker } from 'pixi.js';
+import { Application, Assets, Graphics, Sprite, Ticker, BitmapText, TextStyle, type TextStyleOptions } from 'pixi.js';
 
-let app = new Application();
+export enum Direction
+{
+    Left = 0,
+    Down = 1,
+    Up = 2,
+    Right = 3
+}
+
+export let app = new Application();
+
+let beatsVisible = 5;
+let numSubbeats = 16
 
 export async function setup(container: HTMLElement) {
     await app.init({width: 600, height: 1000 });
     container.appendChild(app.canvas);
 
     const arrows = await Promise.all([
-        createArrow(ArrowDirection.Left),
-        createArrow(ArrowDirection.Down),
-        createArrow(ArrowDirection.Up),
-        createArrow(ArrowDirection.Right),
+        createArrow(Direction.Left),
+        createArrow(Direction.Down),
+        createArrow(Direction.Up),
+        createArrow(Direction.Right),
     ])
 
     arrows.forEach((arrow, index) => {
@@ -19,31 +30,46 @@ export async function setup(container: HTMLElement) {
         app.stage.addChild(arrow);
     })
 
+    await createLines();
+    setupInfo();
+
     // Listen for animate update
     app.ticker.add(loop);
 };
 
-enum ArrowDirection
-{
-    Left,
-    Down,
-    Up,
-    Right
+async function createLines() {
+    let graphics = new Graphics()
+    let margin = 50;
+    for (let i = 0; i < beatsVisible; i++) {
+        let height = margin + (app.screen.height - 2 * margin) / beatsVisible * i
+        graphics
+            .moveTo(0, height)
+            .lineTo(app.screen.width, height)
+            .stroke({ color: "white", pixelLine: true });
+        for (let j = 1; j < numSubbeats; j++) {
+            let height = margin + (app.screen.height - 2 * margin) / beatsVisible * (i + j / numSubbeats)
+            graphics
+                .moveTo(0, height)
+                .lineTo(app.screen.width, height)
+                .stroke({ color: "gray", pixelLine: true });
+        }
+    }
+    app.stage.addChild(graphics);
 }
 
-async function createArrow(arrowDirection: ArrowDirection): Promise<Sprite> {
+async function createArrow(arrowDirection: Direction): Promise<Sprite> {
     let texture
     switch (arrowDirection) {
-        case ArrowDirection.Up:
+        case Direction.Up:
             texture = await Assets.load('/arrow-up.png');
             break;
-        case ArrowDirection.Right:
+        case Direction.Right:
             texture = await Assets.load('/arrow-right.png');
             break;
-        case ArrowDirection.Down:
+        case Direction.Down:
             texture = await Assets.load('/arrow-down.png');
             break;
-        case ArrowDirection.Left:
+        case Direction.Left:
             texture = await Assets.load('/arrow-left.png');
             break;
     }
@@ -55,40 +81,132 @@ async function createArrow(arrowDirection: ArrowDirection): Promise<Sprite> {
     return arrow
 }
 
-function getxPosition(arrowDirection: ArrowDirection): number {
+function getxPosition(arrowDirection: Direction): number {
     return app.screen.width / 8 * (arrowDirection * 2 + 1);
 }
 
 interface Arrow {
     sprite: Sprite,
-    direction: ArrowDirection
+    direction: Direction
 }
+
 
 let queue: Arrow[] = []
 let bpm = 120
+let time = 0
+let beat = 0
+let subbeat = 0
 let timePassedSinceLastArrow = 0
 let timeBetweenArrows = 60 * 1000 / bpm
-async function loop(time: Ticker) {
-    timePassedSinceLastArrow += time.deltaMS
+// advance arrows every 1/16 of a beat
+async function loop(ticker: Ticker) {
+    updateTime(ticker)
+
+    timePassedSinceLastArrow += ticker.deltaMS
 
     // add arrows
     if (timePassedSinceLastArrow > timeBetweenArrows)
     {
         timePassedSinceLastArrow = timePassedSinceLastArrow - timeBetweenArrows
-        let randomArrowDirection = Math.floor(Math.random() * 4)
-        let arrowSprite = await createArrow(randomArrowDirection)
-        arrowSprite.x = getxPosition(randomArrowDirection)
+        let randomDirection = Math.floor(Math.random() * 4)
+        let arrowSprite = await createArrow(randomDirection)
+        arrowSprite.x = getxPosition(randomDirection)
         arrowSprite.y = app.screen.height
         app.stage.addChild(arrowSprite)
-        queue.push({ sprite: arrowSprite, direction: randomArrowDirection })
+        queue.push({ sprite: arrowSprite, direction: randomDirection })
     }
 
     // move arrows
     queue.forEach((arrow: Arrow) => {
-        arrow.sprite.y -= 10 * time.deltaTime
-        if (arrow.sprite.y < 0) {
+        arrow.sprite.y -= 10 * ticker.deltaTime
+        if (arrow.sprite.y < -50) {
             arrow.sprite.destroy()
             queue.shift()
         }
     })
+
+    updateInfo(ticker);
+}
+
+function updateTime(ticker: Ticker) {
+    time += ticker.deltaMS
+    let timeInSeconds = time / 1000
+    let timeInMinutes = timeInSeconds / 60
+    beat = Math.floor(timeInMinutes * bpm)
+    subbeat = Math.floor(((timeInMinutes * bpm) - beat) * numSubbeats)
+}
+
+let fpsText: BitmapText;
+let timeText: BitmapText;
+let beatText: BitmapText;
+let subbeatText: BitmapText;
+function setupInfo() {
+    const fontStyle: TextStyleOptions = {
+        fontSize: 30,
+        align: 'left',
+        fill: "red",
+    }
+    fpsText = new BitmapText({
+        text: "",
+        style: fontStyle,
+    });
+    beatText = new BitmapText({
+        text: "",
+        style: fontStyle,
+    });
+    subbeatText = new BitmapText({
+        text: "",
+        style: fontStyle,
+    });
+    timeText = new BitmapText({
+        text: "",
+        style: fontStyle,
+    });
+
+    fpsText.x = 10
+    timeText.x = 10
+    beatText.x = 10
+    subbeatText.x = 10
+
+    fpsText.y = 10
+    timeText.y = 40
+    beatText.y = 70
+    subbeatText.y = 100
+
+    app.stage.addChild(fpsText)
+    app.stage.addChild(timeText)
+    app.stage.addChild(beatText)
+    app.stage.addChild(subbeatText)
+}
+
+let fpsArray: number[] = []
+let numberFpsSamples = 100
+let fps = 0
+function updateFps(ticker: Ticker) {
+    fpsArray.push(1000 / ticker.deltaMS)
+    if (fpsArray.length > numberFpsSamples) {
+        fpsArray.shift()
+    }
+    fps = fpsArray.reduce((a, b) => a + b, 0) / fpsArray.length
+}
+
+function updateInfo(ticker: Ticker) {
+    updateFps(ticker)
+    fpsText.text = Math.round(fps)
+    let seconds = time / 1000
+    let mintes = Math.floor(seconds / 60)
+    let secondsLeft = Math.floor(seconds - mintes * 60)
+    let secondsLeftString = `${secondsLeft}`
+    if (secondsLeft < 10) {
+        secondsLeftString = "0" + secondsLeftString
+    }
+    let timeString = `${mintes}:${secondsLeftString}`
+    timeText.text = timeString
+    beatText.text = beat
+    let subbeatString = `${subbeat}`
+    if (subbeat < 10) {
+        subbeatString = "0" + subbeat
+    }
+    subbeatText.text = subbeatString
+
 }
