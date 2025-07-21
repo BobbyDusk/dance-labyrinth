1e6d4fdbb193256ef7323ba274f113f07825a35d
Bobby Dusk <bobby@dusk.monster>
2025-07-21 20:35:43 +0200

    trying to solve an issue where the metronome stutters whenever a button is pressed in the pixi canvas using touch

--- frontend/src/Dance/Controls.svelte
+++ frontend/src/Dance/Controls.svelte
@@ -67,150 +67,149 @@
   }
 </script>
 
-<div class="flex flex-col gap-4 p-4">
-  <div>
-    <label for="bpm">bpm: </label>
+<div>
+  <label for="bpm">bpm: </label>
+  <input
+    type="number"
+    id="bpm"
+    bind:value={bpm}
+    min="1"
+    max="300"
+    class="w-16"
+    onchange={() => {
+      metronome.bpm = bpm;
+      danceManager.chart.bpm = bpm;
+      danceManager.updateChart();
+    }}
+  />
+  <p>
+    beat: <span class="inline-block w-8 text-end">{beat}</span>
+    <sup><span class="inline-block w-4 text-end">{subbeat}</span></sup
+    >&frasl;<sub>{Metronome.NUM_SUBBEATS}</sub>
+  </p>
+  <p>time: {new Date(time).toISOString().substring(14, 23)}</p>
+</div>
+<div>
+  <Button onclick={togglePlayPause}>
+    {#if paused}
+      Start
+    {:else}
+      Pause
+    {/if}
+  </Button>
+  <Button onclick={reset}>Reset</Button>
+</div>
+<div>
+  <Button
+    onclick={() => {
+      const fileInput = document.createElement("input");
+      fileInput.type = "file";
+      fileInput.accept = ".chart";
+      fileInput.onchange = (event) => {
+        const target = event.target as HTMLInputElement;
+        if (target.files && target.files.length > 0) {
+          danceManager.loadChart(target.files[0]);
+        }
+      };
+      fileInput.click();
+    }}>Load Chart</Button
+  >
+  <Button
+    onclick={() => {
+      let file = danceManager.saveChart();
+      const url = URL.createObjectURL(file);
+      const a = document.createElement("a");
+      a.href = url;
+      a.download = "song.chart";
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+    }}
+  >
+    Save Chart
+  </Button>
+</div>
+<div>
+  <Button
+    onclick={() => {
+      danceManager.createChart();
+    }}>New Chart</Button
+  >
+  <Button
+    onclick={() => {
+      const input = document.createElement("input");
+      input.style.display = "none";
+      input.type = "file";
+      input.accept = ".mp3,.wav,.ogg";
+      input.onchange = async (event) => {
+        const target = event.target as HTMLInputElement;
+        if (target.files && target.files.length > 0) {
+          danceManager.loadSong(target.files[0]);
+        }
+      };
+      input.click();
+    }}>Load Song</Button
+  >
+</div>
+<div>
+  <label for="snappingInterval" class="text-lg"> Snapping Interval: </label>
+  <select
+    id="snappingInterval"
+    bind:value={snappingInterval}
+    class="w-32"
+    onchange={() => {
+      danceTrack.snappingInterval = snappingInterval;
+    }}
+  >
+    <option value={1}>1 / 64 beat</option>
+    <option value={2}>1 / 32 beat</option>
+    <option value={4}>1 / 16 beat</option>
+    <option value={8}>1 / 8 beat</option>
+    <option value={16}>1 / 4 beat</option>
+    <option value={32}>1 / 2 beat</option>
+    <option value={64}>1 beat</option>
+  </select>
+</div>
+<div>
+  <label for="orientation" class="text-lg"> Orientation: </label>
+  <select
+    id="orientation"
+    bind:value={orientation}
+    class="w-32"
+    onchange={() => {
+      danceTrack.orientation = orientation;
+    }}
+  >
+    <option value={Orientation.DOWN}>Scroll Down</option>
+    <option value={Orientation.UP}>Scroll Up</option>
+  </select>
+</div>
+<div class="flex flex-col gap-1/2">
+  <label for="linesVisible" class="text-lg">
+    Show Lines:
     <input
-      type="number"
-      id="bpm"
-      bind:value={bpm}
-      min="1"
-      max="300"
-      class="w-16"
+      type="checkbox"
+      id="linesVisible"
+      bind:checked={linesVisible}
       onchange={() => {
-        metronome.bpm = bpm;
-        danceManager.chart.bpm = bpm;
-        danceManager.updateChart();
+        danceTrack.linesVisible = linesVisible;
       }}
     />
-    <p>
-      beat: <span class="inline-block w-8 text-end">{beat}</span>
-      <sup><span class="inline-block w-4 text-end">{subbeat}</span></sup
-      >&frasl;<sub>{Metronome.NUM_SUBBEATS}</sub>
-    </p>
-    <p>time: {new Date(time).toISOString().substring(14, 23)}</p>
-  </div>
-  <div>
-    <Button onclick={togglePlayPause}>
-      {#if paused}
-        Start
-      {:else}
-        Pause
-      {/if}
-    </Button>
-    <Button onclick={reset}>Reset</Button>
-  </div>
-  <div>
-    <Button
-      onclick={() => {
-        const fileInput = document.createElement("input");
-        fileInput.type = "file";
-        fileInput.accept = ".chart";
-        fileInput.onchange = (event) => {
-          const target = event.target as HTMLInputElement;
-          if (target.files && target.files.length > 0) {
-            danceManager.loadChart(target.files[0]);
-          }
-        };
-        fileInput.click();
-      }}>Load Chart</Button
-    >
-    <Button
-      onclick={() => {
-        let file = danceManager.saveChart();
-        const url = URL.createObjectURL(file);
-        const a = document.createElement("a");
-        a.href = url;
-        a.download = "song.chart";
-        document.body.appendChild(a);
-        a.click();
-        document.body.removeChild(a);
-      }}
-    >
-      Save Chart
-    </Button>
-  </div>
-  <div>
-    <Button
-      onclick={() => {
-        danceManager.createChart();
-      }}>New Chart</Button
-    >
-    <Button
-      onclick={() => {
-        const input = document.createElement("input");
-        input.style.display = "none";
-        input.type = "file";
-        input.accept = ".mp3,.wav,.ogg";
-        input.onchange = async (event) => {
-          const target = event.target as HTMLInputElement;
-          if (target.files && target.files.length > 0) {
-            danceManager.loadSong(target.files[0]);
-          }
-        };
-        input.click();
-      }}>Load Song</Button
-    >
-  </div>
-  <div>
-    <label for="snappingInterval" class="text-lg"> Snapping Interval: </label>
-    <select
-      id="snappingInterval"
-      bind:value={snappingInterval}
-      class="w-32"
+  </label>
+  <label for="waveformVisible" class="text-lg">
+    Show Waveform:
+    <input
+      type="checkbox"
+      id="waveformVisible"
+      bind:checked={waveformVisible}
       onchange={() => {
-        danceTrack.snappingInterval = snappingInterval;
+        danceTrack.waveformVisible = waveformVisible;
       }}
-    >
-      <option value={1}>1 / 64 beat</option>
-      <option value={2}>1 / 32 beat</option>
-      <option value={4}>1 / 16 beat</option>
-      <option value={8}>1 / 8 beat</option>
-      <option value={16}>1 / 4 beat</option>
-      <option value={32}>1 / 2 beat</option>
-      <option value={64}>1 beat</option>
-    </select>
-  </div>
-  <div>
-    <label for="orientation" class="text-lg"> Orientation: </label>
-    <select
-      id="orientation"
-      bind:value={orientation}
-      class="w-32"
-      onchange={() => {
-        danceTrack.orientation = orientation;
-      }}
-    >
-      <option value={Orientation.DOWN}>Scroll Down</option>
-      <option value={Orientation.UP}>Scroll Up</option>
-    </select>
-  </div>
-  <div class="flex flex-col gap-1/2">
-    <label for="linesVisible" class="text-lg">
-      Show Lines:
-      <input
-        type="checkbox"
-        id="linesVisible"
-        bind:checked={linesVisible}
-        onchange={() => {
-          danceTrack.linesVisible = linesVisible;
-        }}
-      />
-    </label>
-    <label for="waveformVisible" class="text-lg">
-      Show Waveform:
-      <input
-        type="checkbox"
-        id="waveformVisible"
-        bind:checked={waveformVisible}
-        onchange={() => {
-          danceTrack.waveformVisible = waveformVisible;
-        }}
-      />
-    </label>
-  </div>
+    />
+  </label>
+</div>
 
-  <!--
+<!--
   <div>
     <label for="scale" class="text-lg"> scale: </label>
     <input
@@ -223,4 +222,3 @@
     />
   </div>
   -->
-</div>
--- frontend/src/Dance/inputSetup.ts
+++ frontend/src/Dance/inputSetup.ts
@@ -1,5 +1,5 @@
 import { MoveDirection, setActiveDirection, activeDirection } from "../labyrinthApp"
-import { inputManager, KeyPress } from "../Input"
+import { inputManager, KeyPress, Input } from "../Input"
 import { danceManager } from "./DanceManager";
 
 export default function setupInput() {
@@ -27,43 +27,4 @@ export default function setupInput() {
   inputManager.addInputGroup("Lane3");
   inputManager.addInput("Lane3", new KeyPress(";"));
   inputManager.addAction("Lane3", () => danceManager.press(3));
-
-  /*
-  document.addEventListener("keydown", (event: KeyboardEvent) => {
-    if (event.key === "j") {
-      press(Direction.Left)
-    } else if (event.key === "k") {
-      press(Direction.Down)
-    } else if (event.key === "l") {
-      press(Direction.Up)
-    } else if (event.key === ";") {
-      press(Direction.Right)
-    } else if (event.key === "a") {
-      setActiveDirection(MoveDirection.Left)
-    } else if (event.key === "s") {
-      setActiveDirection(MoveDirection.Down)
-    } else if (event.key === "w") {
-      setActiveDirection(MoveDirection.Up)
-    } else if (event.key === "d") {
-      setActiveDirection(MoveDirection.Right)
-    }
-  })
-
-  document.addEventListener("keyup", (event: KeyboardEvent) => {
-    if (event.key === "a" || event.key === "s" || event.key === "w" || event.key === "d") {
-      const keyDirectionMap: { [key: string]: MoveDirection } = {
-        a: MoveDirection.Left,
-        s: MoveDirection.Down,
-        w: MoveDirection.Up,
-        d: MoveDirection.Right,
-      };
-
-      const isActiveKey = keyDirectionMap[event.key] === activeDirection;
-      if (isActiveKey) {
-        setActiveDirection(MoveDirection.None);
-      }
-    }
-  })
-  */
-
 }
